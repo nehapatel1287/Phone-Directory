742bc89504925bed76c8b3ca3b026a7cd51e9585
Neha Patel <nehap.1287@gmail.com>
2022-01-24 10:12:05 +0530

    Created Skeleton of Home Page

--- phone-directory/src/App.js
+++ phone-directory/src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        Phone Directory
+      </div>
+      <button> Add</button>
+      <div>
+        <span>Name</span>
+        <span>Phone Number</span>
+      </div>
     </div>
   );
 }
