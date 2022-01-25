45673bdd9838beaa69ab2a2af1d0629599aa5a3c
Neha Patel <nehap.1287@gmail.com>
2022-01-25 18:32:51 +0530

    Rendered Subscribers Dynamically

--- phone-directory/src/App.js
+++ phone-directory/src/App.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
 import Header from './Header';
 
+
 class App extends Component {
   render(){
+    let subscriber=[
+      {
+      id:1,
+      name:"Neha Patel",
+      phone:"9999999999"
+    },
+    {
+      id:2,
+      name:"Suyash Patel",
+      phone:"8888888888"
+    }
+
+  ]
+    
   return (
     <div>
    <Header/>
-      <button> Add</button>
-      <div>
-        <span>Name</span>
-        <span>Phone Number</span>
-      </div>
-    </div>
+   <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+          {
+            subscriber.map(sub=>{
+          return <div key={sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+                <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn">Delete</button>
+                </span>
+              </div>
+              })
+           }
+          </div>
+
+          </div>
   );
 }
 }
---
+++ phone-directory/src/Button.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+ const Button=function() {
+    return (
+        <button className="btn"> Add</button>
+    )
+ }
+ export default Button;
